46887208d0edc9a7201adaf9a6a05d2720d68485
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-02-04 20:38:43 +0100

    Update end of synchronization
    Add easter egg for 418:Teapot

--- Design/syncData.js
+++ Design/syncData.js
@@ -82,13 +82,74 @@ function startSync(){
 	statusBar.innerHTML = "Creating packet"
 	var packet = {
 		"USER_ID": user_id,
-		"DATE": fDate
+		"DATE": fDate,
+		"RECORD": record
 	}
+	packet = JSON.stringify(packet);
 
 
 	statusBar.style.color = 'white'
-	statusBar.innerHTML = "Opening HTTP Request";
+	statusBar.innerHTML = "Creating HTTP Request";
 	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState != 4){
+			statusBar.innerHTML = "Waiting...";
+		}
+		if (this.readyState == 4 && this.status == 200){
+			//All good!
+			statusBar.innerHTML = "";
+			endSync();
+		}
+		if (this.readyState == 4 && this.status != 200){
+			statusBar.innerHTML = "synchronization error...";
+			errSync(this.status);
+		}
+	}
+	statusBar.innerHTML = "Opening HTTP Request";
+	xhttp.open("POST", post_link);
+	statusBar.innerHTML = "Sending packet over HTTP";
+	xhttp.send(packet);
 
 
+}
+
+// After sync success
+function endSync(){
+	//erase all records
+	localStorage.setItem("RECORD", "")
+
+	txtSync.innerHTML = "Synchronization successful."
+	btnSync.innerHTML = btnSyncDone;
+	btnSync.style.color = "green";
+
+	setTimeout(eraseStatusBar, 1000);
+}
+
+// Error sync...
+function errSync(code){
+	btnSync.innerHTML = btnSyncProblemIco;
+	btnSync.style.color = "red";
+
+	switch (code){
+	case 404:
+		txtSync.innerHTML = 'Synchronization failed (404) you should check the "POST_LINK" parameter in the settings.';
+		break;
+	case 401:
+		txtSync.innerHTML = 'Synchronization failed (401) you should check the "USER_ID" parameter in the settings.';
+		break;
+	case 418:
+		txtSync.innerHTML = 'Synchronization failed (418) i refuse to prepare coffee because I am a teapot!';
+		btnSync.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="currentColor"><path d="M8 42v-3h31.95v3Zm7.55-6q-3.15 0-5.35-2.175Q8 31.65 8 28.5V6h9.85v4.65l-3.6 2.9q-.05.05-.45.9v7.5q0 .5.35.85t.9.35h7.55q.55 0 .9-.35t.35-.85v-7.5q0-.25-.45-.9l-3.55-2.9V6H41q1.25 0 2.125.875T44 9v8q0 1.25-.875 2.125T41 20h-4.8v8.5q0 3.15-2.2 5.325Q31.8 36 28.65 36ZM36.2 17H41V9h-4.8Z"/></svg>'
+		btnSync.style.color = "purple";
+		break;
+	default:
+		txtSync.innerHTML = `Synchronization failed (${code}).`;
+	}
+
+	setTimeout(eraseStatusBar, 1000);
+}
+
+// Erase statusBar
+function eraseStatusBar(){
+	statusBar.innerHTML = "";
 }
