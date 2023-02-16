883890e093b946e000877a79eaf760087aa3c865
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-02-16 19:01:29 +0100

    Update Button.js

--- Design/02-REACT-Native/SmartChrono/components/Button.js
+++ Design/02-REACT-Native/SmartChrono/components/Button.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Icons from '../assets/svg/Material_icons';
+import Icons from '../assets/svg/Icons';
 
 export default function Button(props) {
 	const ico = props.ico;
 	const color = props.ico;
-	
+	const size = props.size;
+
+	return (
+		<View style={{width:{size}, height:{size}, backgroundColor:{color}, borderRadius:'50%'}}>
+			<Icons ico={ico} size={size} color='#FFF'/>
+		</View>
+		)
+
 }
