ad33c7cdb5608aecd793b7e56dd843c67883d005
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-02-17 12:14:16 +0100

    Update TabChrono.js

--- Design/02-REACT-Native/SmartChrono/components/tabs/TabChrono.js
+++ Design/02-REACT-Native/SmartChrono/components/tabs/TabChrono.js
@@ -2,3 +2,16 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Button from '../Button'
 
+export default function TabChrono() {
+
+	return (
+		<View style={styles.container}>
+			
+		</View>
+		)
+
+}
+
+const styles = StyleSheet.create({
+	container: {}
+})
