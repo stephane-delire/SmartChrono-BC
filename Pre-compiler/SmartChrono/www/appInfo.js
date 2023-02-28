806b21bd4f373de644b98d783510bd1cba7c5b4c
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-02-28 17:51:59 +0100

    Removing files from react & cordova

--- Pre-compiler/SmartChrono/www/appInfo.js
+++
@@ -1,9 +0,0 @@
-/*////////////////////////////////////////////////////////*/
-//Version
-//localStorage.setItem("VERSION", "0.0.1");
-
-/*
-Note for later,
-need to use cordova-plugin-device
-https://cordova.apache.org/docs/en/11.x/reference/cordova-plugin-device/
-*/
