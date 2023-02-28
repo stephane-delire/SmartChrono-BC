91d680dbb84f85cc63c290f16d35c49434dd9ec0
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-02-28 22:29:18 +0100

    Create every views for login

--- Design/03-SwiftUI/SmartChrono-BC/SmartChrono-BC/login.swift
+++ Design/03-SwiftUI/SmartChrono-BC/SmartChrono-BC/login.swift
@@ -11,9 +11,21 @@ struct login: View {
     
     @State var userLogin:String = ""
     @State var userPassword:String = ""
+    @State var userSettingUrl:String = ""
+    @State var userSettingDB:String = ""
     
     @State var userShowPassword:Bool = false
     
+    @State var loginState:Int = 6
+    // 0 : Login
+    // 1 : Settings
+    // 2 : Working Settings
+    // 3 : Error saving settings
+    // 4 : Settings recorded
+    // 5 : Try To authenticate
+    // 6 : Success
+    // 7 : Error authentication
+//-----------------------------------------
     func loginToggleShowPassword(){
         if userShowPassword == false {
             userShowPassword = true
@@ -22,102 +34,375 @@ struct login: View {
         }
     }
     
-    var body: some View {
-        VStack{
-            
-            //Image logo
-            Image("Logo")
-                .resizable()
-                .scaledToFit()
-            
-            HStack{
-                Text("Login")
-                    .foregroundColor(Color("Greys"))
-                    .multilineTextAlignment(.leading)
-                Spacer()
-            }
-            .frame(width: 315)
-            TextField("Login...", text:$userLogin)
-                .padding()
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
-                .overlay(VStack{
-                    Divider()
-                        .overlay(Color("Greys"))
-                        .frame(width: 315)
-                        .offset(x:0, y:15)
-                        
-                })
-            
-            
-            HStack{
-                Text("Password")
-                    .foregroundColor(Color("Greys"))
-                    .multilineTextAlignment(.leading)
-                Spacer()
-                
-                if userShowPassword {
-                    Button(action: loginToggleShowPassword){
-                        Image(systemName: "eye.slash")
-                            .foregroundColor(Color("Turquoise"))
-                    }
-                } else {
-                    Button(action: loginToggleShowPassword){
-                        Image(systemName: "eye")
-                            .foregroundColor(Color("Turquoise"))
-                    }
-                }
-            }
-            .frame(width: 315)
-            
-            //Show hide password
-            if userShowPassword == false {
-                SecureField("Password...", text:$userPassword)
-                    .padding()
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .overlay(VStack{
-                        Divider()
-                            .overlay(Color("Greys"))
-                            .frame(width: 315)
-                            .offset(x:0, y:15)
-                            
-                    })
-            } else {
-                TextField("Password...", text: $userPassword)
-                    .padding()
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .overlay(VStack{
-                        Divider()
-                            .overlay(Color("Greys"))
-                            .frame(width: 315)
-                            .offset(x:0, y:15)
-                            
-                    })
-            }
-            
-            Button(action: dummy){
-                Text("LOGIN")
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.white)
-                    .background(Color("Turquoise"))
-                    .frame(width:315, height: 32)
-                    .cornerRadius(16)
-                
-            }
-            .cornerRadius(16)
-            .background(Color("Turquoise"))
-            .padding(.top, 32)
-            
-            
-            Spacer()
-            
-        }
-        .padding()
-        .frame(width: 375)
+    func hapticVibrateSuccess(){
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
     }
-}
+    func hapticVibrateError(){
+        UINotificationFeedbackGenerator().notificationOccurred(.error)
+    }
+    
+    var body: some View {
+        
+        //Login page
+        if loginState == 0 {
+            Group{
+                VStack{
+                    
+                    //Image logo
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    
+                    HStack{
+                        Text("Login")
+                            .foregroundColor(Color("Greys"))
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                    }
+                    .frame(width: 315)
+                    .padding(.bottom, -10)
+                    TextField("Login...", text:$userLogin)
+                        .padding()
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .overlay(VStack{
+                            Divider()
+                                .overlay(Color("Greys"))
+                                .frame(width: 315)
+                                .offset(x:0, y:15)
+                            
+                        })
+                    
+                    
+                    HStack{
+                        Text("Password")
+                            .foregroundColor(Color("Greys"))
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                        
+                        if userShowPassword {
+                            Button(action: loginToggleShowPassword){
+                                Image(systemName: "eye.slash")
+                                    .foregroundColor(Color("Turquoise"))
+                            }
+                        } else {
+                            Button(action: loginToggleShowPassword){
+                                Image(systemName: "eye")
+                                    .foregroundColor(Color("Turquoise"))
+                            }
+                        }
+                    }
+                    .frame(width: 315)
+                    .padding(.bottom, -10)
+                    
+                    //Show hide password
+                    if userShowPassword == false {
+                        SecureField("Password...", text:$userPassword)
+                            .padding()
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .overlay(VStack{
+                                Divider()
+                                    .overlay(Color("Greys"))
+                                    .frame(width: 315)
+                                    .offset(x:0, y:15)
+                                
+                            })
+                    } else {
+                        TextField("Password...", text: $userPassword)
+                            .padding()
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .overlay(VStack{
+                                Divider()
+                                    .overlay(Color("Greys"))
+                                    .frame(width: 315)
+                                    .offset(x:0, y:15)
+                                
+                            })
+                    }
+                    
+                    Button(action: dummy){
+                        Text("LOGIN")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                            .background(Color("Turquoise"))
+                            .frame(width:315, height: 32)
+                            .cornerRadius(16)
+                        
+                    }
+                    .cornerRadius(16)
+                    .background(Color("Turquoise"))
+                    .padding(.top, 32)
+                    
+                    Button(action: dummy){
+                        Image(systemName: "gear")
+                            .foregroundColor(Color.white)
+                            .background(Color("Greys"))
+                            .frame(width: 315, height: 32)
+                    }
+                    .cornerRadius(16)
+                    .background(Color("Greys"))
+                    
+                    Spacer()
+                    
+                }
+                .padding()
+                .frame(width: 375)
+            }
+        }
+        
+        // Settings
+        if loginState == 1 {
+            Group {
+                VStack {
+                    //Image logo
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    
+                    HStack{
+                        Text("Url")
+                            .foregroundColor(Color("Greys"))
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                    }
+                    .frame(width: 315)
+                    .padding(.bottom, -10)
+                    
+                    TextField("Url...", text:$userSettingUrl)
+                        .padding()
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .overlay(VStack{
+                            Divider()
+                                .overlay(Color("Greys"))
+                                .frame(width: 315)
+                                .offset(x:0, y:15)
+                            
+                        })
+                    
+                    HStack{
+                        Text("Database")
+                            .foregroundColor(Color("Greys"))
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                    }
+                    .frame(width: 315)
+                    .padding(.bottom, -10)
+                    
+                    TextField("Url...", text:$userSettingDB)
+                        .padding()
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .overlay(VStack{
+                            Divider()
+                                .overlay(Color("Greys"))
+                                .frame(width: 315)
+                                .offset(x:0, y:15)
+                            
+                        })
+                    
+                    Button(action: dummy){
+                        Text("SAVE")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                            .background(Color("Turquoise"))
+                            .frame(width:315, height: 32)
+                            .cornerRadius(16)
+                        
+                    }
+                    .cornerRadius(16)
+                    .background(Color("Turquoise"))
+                    .padding(.top, 32)
+                    
+                    Button(action: dummy){
+                        Image(systemName: "arrow.backward")
+                            .foregroundColor(Color.white)
+                            .background(Color("Greys"))
+                            .frame(width: 315, height: 32)
+                    }
+                    .cornerRadius(16)
+                    .background(Color("Greys"))
+                    
+                    Spacer()
+                }
+                .padding()
+                .frame(width: 375)
+            }
+        }
+        
+        // Working Settings
+        if loginState == 2 {
+            Group{
+                VStack{
+                    //Image logo
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    ProgressView()
+                    
+                    Spacer()
+                }
+                .padding()
+                .frame(width: 375)
+            }
+        }
+        
+        // Error saving Settings
+        if loginState == 3 {
+            Group{
+                VStack{
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Spacer()
+                    
+                    Image(systemName: "exclamationmark.triangle")
+                        .font(.system(size: 40))
+                        .foregroundColor(Color.red)
+                    Text("Unable to save settings.")
+                        .foregroundColor(Color.red)
+                    
+                    Spacer()
+                }
+                .padding()
+                .frame(width: 375)
+                .onAppear{hapticVibrateError()}
+            }
+        }
+        
+        // Success saving settings
+        if loginState == 4 {
+            Group{
+                VStack{
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Image(systemName: "checkmark.circle")
+                        .font(.system(size: 40))
+                        .foregroundColor(Color("Turquoise"))
+                    Text("Settings saved.")
+                    
+                    Spacer()
+                    
+                    Button(action: dummy){
+                        Image(systemName: "arrow.backward")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                            .background(Color("Greys"))
+                            .frame(width:315, height: 32)
+                            .cornerRadius(16)
+                        
+                    }
+                    .cornerRadius(16)
+                    .background(Color("Greys"))
+                    .padding(.top, 32)
+                    
+                    Spacer()
+                    
+                }
+                .padding()
+                .frame(width: 375)
+                .onAppear{hapticVibrateSuccess()}
+            }
+        }
+        
+        // Authenticate
+        if loginState == 5 {
+            Group{
+                VStack{
+                    //Image logo
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    ProgressView()
+                    
+                    //Spacer()
+                    
+                    Text("Authentication.")
+                        .foregroundColor(Color("Greys"))
+                    
+                    Spacer()
+                }
+                .padding()
+                .frame(width: 375)
+            }
+        }
+        
+        // Error authentication
+        if loginState == 7 {
+            Group{
+                VStack{
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Spacer()
+                    
+                    Image(systemName: "exclamationmark.triangle")
+                        .font(.system(size: 40))
+                        .foregroundColor(Color.red)
+                    Text("Identification problem.\nPlease check your Login and Password.")
+                        .foregroundColor(Color.red)
+                        .multilineTextAlignment(.center)
+                    
+                    Button(action: dummy){
+                        Image(systemName: "arrow.backward")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                            .background(Color("Greys"))
+                            .frame(width:315, height: 32)
+                            .cornerRadius(16)
+                        
+                    }
+                    .cornerRadius(16)
+                    .background(Color("Greys"))
+                    .padding(.top, 32)
+                    
+                    Spacer()
+                }
+                .padding()
+                .frame(width: 375)
+                .onAppear{hapticVibrateError()}
+            }
+        }
+        
+        // Success authentication
+        if loginState == 6 {
+            Group{
+                VStack{
+                    Image("Logo")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .font(.system(size: 40))
+                        .foregroundColor(Color("Turquoise"))
+                    Text("Success.")
+                    
+                    Spacer()
+                    
+                    ProgressView()
+                    
+                    Spacer()
+                    
+                }
+                .padding()
+                .frame(width: 375)
+                .onAppear{hapticVibrateSuccess()}
+            }
+        }
+        
+        
+    }//End view
+}//End Struct
 
 struct login_Previews: PreviewProvider {
     static var previews: some View {
