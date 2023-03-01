86cc03f32cb181e08abe50f383897340d7253280
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-03-01 18:05:48 +0100

    Create Class User for authenticate

---
+++ Design/03-SwiftUI/SmartChrono-BC/SmartChrono-BC/Codes/User.swift
@@ -0,0 +1,62 @@
+//
+//  User.swift
+//  SmartChrono-BC
+//
+//  Created by Delire Stéphane on 01/03/2023.
+//
+
+import Foundation
+
+class User: Codable {
+    
+    var login: String = ""
+    var password: String = "" // Will need this for FaceID ;)
+    var id:Int = 0
+    
+    
+    init() {
+        loadUser()
+    }
+    
+    
+    func loadUser(){
+        var fileURL:URL
+        let fm = FileManager.default
+        let dir = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        if dir.count != 0 {
+            fileURL = dir[0].appendingPathComponent("user.json")
+        } else {
+            print("class User - unable to obtain a valid file path")
+            return;
+        }
+        
+        if let data = try? Data(contentsOf: fileURL), let user = try? JSONDecoder().decode(User.self, from: data) {
+            self.login = user.login
+            self.password = user.password
+            self.id = user.id
+        } else {
+            self.login = ""
+            self.password = ""
+            self.id = 0
+        }
+    }
+    
+    func saveUser(){
+        var fileURL:URL
+        let fm = FileManager.default
+        let dir = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        if dir.count != 0 {
+            fileURL = dir[0].appendingPathComponent("user.json")
+        } else {
+            print("class User - unable to obtain a valid file path")
+            return;
+        }
+        
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        if let data = try? encoder.encode(self) {
+            try? data.write(to: fileURL)
+        }
+    }
+    
+}
